1abc91a49b56240d0bef10cd7d5f57bf71d8d9b5
andy-shi88 <shi88.andy@gmail.com>
2019-05-04 19:13:25 +0800

    update reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,24 +2,12 @@ import { combineReducers } from 'redux'
 
 import profile from './profile/profile.reducers'
 import users from './users/users.reducers'
-import hotels from './hotels/hotel.reducers'
-import rooms from './rooms/room.reducers'
-import hotel_books from './hotel_books/hotel_book.reducers'
-import hotel_price_rules from './hotel_price_rules/hotel_price_rule.reducers'
-import eventCategories from './events/category.reducers'
-import eventPolicies from './events/policy.reducers'
-import events from './events/event.reducers'
+import indicators from './indicator/indicator.reducers'
 
 const reducers = combineReducers({
   profile,
   users,
-  hotels,
-  rooms,
-  hotel_books,
-  hotel_price_rules,
-  eventCategories,
-  eventPolicies,
-  events,
+  indicators,
 })
 
 export default reducers
