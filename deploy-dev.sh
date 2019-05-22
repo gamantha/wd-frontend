afe643844bead5933a6b8851b72e0eed97768536
Samsul Arifin <arifinsam.sam@gmail.com>
2019-05-22 20:16:30 +0700

    updatedir deploy

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,5 +1,5 @@
 echo "deploying build folder to wd.gamantha.com..."
 
-scp -r build/. wduser@gamantha.com:/var/www/html/walkingdocs
+scp -r build/* wduser@aws.ppsdm.com:/var/www/html/walkingdocs/wd-frontend
 
 echo "deployed."
